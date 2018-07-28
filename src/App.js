ee9811b103b0ee76a00bf268ac68e56d5282cf9a
dinhvietan <dinhvietan33@gmail.com>
2018-07-28 19:55:55 +0700

    123

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import './App.css';
 import Clock from './Components/Clock.js';
 
 
-// class Clock extends Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {message: "Welcome"};
-//     this.onChange= this.onChange.bind(this);
-//   }
-//   onChange(event){
-//     this.setState({
-//       message: "ABCD"
-//     });
-//   }
-//   render() {
-//     return(
-//       <div>
-//           <h1>Hello World</h1>
-//               <h3>{this.props.title}</h3>
-//               <h2>It is {this.state.message}.</h2>
-//               <button onClick={this.onChange}>Click me</button>
-//         </div>
-//     );
-//   }
-//}
-
-
 
 class App extends Component {
   render() {
