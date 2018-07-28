a42ad67d31a807cd40131ac251735ba00dff0038
dinhvietan <dinhvietan33@gmail.com>
2018-07-28 19:35:16 +0700

    Up

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import LoginFrom from './LoginForm';
-import NavBar from './Navbar';
+import Clock from './Components/Clock.js';
 
-class Clock extends Component{
-  constructor(props){
-    super(props);
-    this.state = {message: "Welcome"};
-    this.onChange= this.onChange.bind(this);
-  }
-  onChange(event){
-    this.setState({
-      message: "ABCD"
-    });
-  }
-  render() {
-    return(
-      <div>
-          <h1>Hello World</h1>
-              <h3>{this.props.title}</h3>
-              <h2>It is {this.state.message}.</h2>
-              <button onClick={this.onChange}>Click me</button>
-        </div>
-    );
-  }
-}
+
+// class Clock extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {message: "Welcome"};
+//     this.onChange= this.onChange.bind(this);
+//   }
+//   onChange(event){
+//     this.setState({
+//       message: "ABCD"
+//     });
+//   }
+//   render() {
+//     return(
+//       <div>
+//           <h1>Hello World</h1>
+//               <h3>{this.props.title}</h3>
+//               <h2>It is {this.state.message}.</h2>
+//               <button onClick={this.onChange}>Click me</button>
+//         </div>
+//     );
+//   }
+//}
 
 
 
@@ -33,8 +33,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Clock></Clock>
-      <LoginFrom></LoginFrom>
+      <Clock name="Javascipt"></Clock>
       </div>
     );
   }
